91c93f0923052af72b609df6795fdde8815d738f
mayu.ito <shigmastudy@gmail.com>
2021-03-08 13:03:16 +0000

    kadai-tasklist

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,9 +47,16 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        //バリデーション
+        $request->validate([
+            'content' => 'required',
+            'status' => 'required|max:10',
+        ]);
+        
         // メッセージを作成
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
 
         // トップページへリダイレクトさせる
@@ -99,10 +106,18 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+       //バリデーション
+        $request->validate([
+            'content' => 'required',
+            'status' => 'required|max:10',
+        ]);
+        
         //// idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
+        
         // メッセージを更新
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
 
         // トップページへリダイレクトさせる
