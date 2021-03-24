7b2227b4d9f6e2e0a2653af718d3a725fb43346c
mayu.ito <shigmastudy@gmail.com>
2021-03-24 12:08:02 +0000

    post_re

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -16,8 +16,7 @@
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
                         <ul class="dropdown-menu dropdown-menu-right">
                             {{-- ユーザ詳細ページへのリンク --}}
-                            <li class="dropdown-item">{!! link_to_route('tasks.index', 'My tasks', ['task' => $task->id]) !!}</li>
-                            <!--<li class="dropdown-item"><a href="#">My tasks</a></li>-->
+                            <li class="dropdown-item"><a href="#">My tasks</a></li>
                             <li class="dropdown-divider"></li>
                             {{-- ログアウトへのリンク --}}
                             <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
